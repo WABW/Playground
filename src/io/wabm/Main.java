f2b08f5cfb27db44f98e815b26c210eaace7d5c0
CMK <cirno.mainasuk@gmail.com>
2017-03-06 15:29:00 +0800

    - init

---
+++ src/io/wabm/Main.java
@@ -0,0 +1,11 @@
+package io.wabm;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.println("Do anything you like");
+
+        System.out.println("Hi");
+    }
+}
